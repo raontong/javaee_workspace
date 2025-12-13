5430c95733321abb7cdfcfa20e6ebe55ddadbf44
raontong <raontong@gmail.com>
2025-12-13 11:34:33 +0900

    314

--- mvcframework/src/main/webapp/js/Box.js
+++ mvcframework/src/main/webapp/js/Box.js
@@ -12,6 +12,7 @@ class Box{
         this.height=height;
         this.bg=bg;
         this.msg=msg;
+        this.dd; // 박스가 보유할 날짜(printNum함수로 이중 반복문돌때 날짜를 주입)
 
         // 스타일적용
         this.div.style.position="absolute";
@@ -31,6 +32,37 @@ class Box{
         // 화면에 부착
         this.container.appendChild(this.div);
 
+        // 이벤트 연결(마우스 오버)
+        this.div.addEventListener("mouseover", ()=>{
+            // 화살표 함수에서의 this 상위 스코프를 가리키므로, 현재 메서드에ㅐ서의 상위 스코프는 BOx라는 객체를맗낟.ㅏ
+            this.div.style.background="";
+        });
+        this.div.addEventListener("mouseout", ()=>{
+            // 화살표 함수에서의 this 상위 스코프를 가리키므로, 현재 메서드에ㅐ서의 상위 스코프는 BOx라는 객체를맗낟.ㅏ
+            this.div.style.background="yellow";
+        });
+
+        //클릭 이벤트 연결
+        this.div.addEventListener("click", ()=>{
+            // 화살표 함수에서의 this 상위 스코프를 가리키므로, 현재 메서드에ㅐ서의 상위 스코프는 BOx라는 객체를맗낟.ㅏ
+            alert(currentDate.getFullYear()+"년" + (currentDate.getMonth()+1)+"월"+this.dd+"일입니다");
+        });
+
+
     }
-            
+
+    //텍스트 넣기
+    setMsg(msg){
+        this.div.innerText = msg;
+        this.msg = msg;            
+    }
+
+    // 날짜 대입하기
+    setDate(dd){
+        this.dd=dd;
+    }
+
 }
+
+    
+            
